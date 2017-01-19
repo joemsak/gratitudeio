e9bd96a36380c46d960b29fb542cbe97907e7bb4
Joe Sak <joe@joesak.com>
2017-01-18 21:58:31 -0600

    Store the values in firebase

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,18 @@ var express = require("express"),
     firebase = require("firebase"),
     router = express.Router();
 
+var config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DB_URL,
+  storageBucket: process.env.FIREBASE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID,
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
 router.get("/", function(req, res) {
   res.status(404).send("404 - Resource not found.")
 });
@@ -13,20 +25,10 @@ router.post("/sms", function(req, res) {
   if (req.body.From !== "+15179445230")
     res.end();
 
-  var config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DB_URL,
-    storageBucket: process.env.FIREBASE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID,
-  };
-  firebase.initializeApp(config);
-
-  var database = firebase.database(),
-      id = database.ref().child('gratitudes').push().key;
-
+  var id = database.ref().child('gratitudes').push().key;
   database.ref("/gratitudes/" + id).set({
     text: req.body.Body,
+    gratefulOn: firebase.database.ServerValue.TIMESTAMP,
   });
 
   res.end();
